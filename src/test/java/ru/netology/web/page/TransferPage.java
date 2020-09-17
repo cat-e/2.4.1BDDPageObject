0a036d767c001bf1ded899a178ec29c91b2f4557
cat-e <khlebalova.e@yandex.ru>
2020-09-17 18:43:56 +0300

    Test updated

--- src/test/java/ru/netology/web/page/TransferPage.java
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -4,33 +4,18 @@ import com.codeborne.selenide.SelenideElement;
 import lombok.Value;
 
 import static com.codeborne.selenide.Selenide.$;
-import static ru.netology.web.data.DataHelper.getFirstCardInfo;
-import static ru.netology.web.data.DataHelper.getSecondCardInfo;
 
 @Value
 public class TransferPage {
 
-
-    private static int amount;
-
-    public static int getAmount() {
-        return amount;
-    }
-
-
-
-    private SelenideElement amountField = $("[data-test-id=amount] input");
-    private SelenideElement fromField = $("[data-test-id=from");
-    private SelenideElement toField = $("[data-test-id=to");
+    private SelenideElement amountField = $("[data-test-id='amount'] input");
+    private SelenideElement fromField = $("[data-test-id='from'] input");
     private SelenideElement transferButton = $("[data-test-id=action-transfer]");
 
-    public TransferPage moneyTransfer(Integer amount, String card) {
-        amountField.setValue(String.valueOf(TransferPage.getAmount()));
-        fromField.setValue(String.valueOf(getFirstCardInfo()));
-//        toField.setValue(String.valueOf(getSecondCardInfo()));
+    public void moneyTransfer(int amount, String card) {
+        amountField.setValue(String.valueOf(amount));
+        fromField.setValue(card);
         transferButton.click();
-        return new TransferPage();
-
     }
 
 
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web.test;
 
 import lombok.val;
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
@@ -8,6 +9,8 @@ import ru.netology.web.page.LoginPage;
 import ru.netology.web.page.TransferPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
 import static ru.netology.web.data.DataHelper.getFirstCardInfo;
 import static ru.netology.web.data.DataHelper.getSecondCardInfo;
 import static ru.netology.web.page.DashboardPage.*;
@@ -16,22 +19,26 @@ class MoneyTransferTest {
 
     @Test
     void shouldLogin() {
+        int amount = 1000;
         open("http://localhost:9999");
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
         val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
-
-        val dashboardPage = new DashboardPage();
+        val dashboardPage = verificationPage.validVerify(verificationCode);
         val firstCardBalance = dashboardPage.getCardBalance(getFirstCardInfo());
         val secondCardBalance = dashboardPage.getCardBalance(getSecondCardInfo());
-        dashboardPage.replenishButtonClick(getSecondCardInfo());
-
-
-        val transferPage = new TransferPage();
-        transferPage.moneyTransfer(1000, "5559 0000 0000 0001");
+        val transferPage = dashboardPage.replenishButtonClick(getSecondCardInfo());
+        transferPage.moneyTransfer(amount, getFirstCardInfo().getCardNumber());
+        val dashboardPageAfterTransfer = new DashboardPage();
+        dashboardPageAfterTransfer.getCardBalance(getFirstCardInfo());
+        assertThat(dashboardPageAfterTransfer, containsString(String.valueOf(getFirstCardInfo())));
 
     }
 
+    private void assertThat(DashboardPage dashboardPageAfterTransfer, Matcher<String> containsString) {
+    }
+
 }
+
+
